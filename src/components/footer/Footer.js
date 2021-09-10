6fcc7de1072d7b76b345ed3bbb2208b47c20acf8
Manoel Pereira <manoelpereiraeng@gmail.com>
2021-09-10 16:13:32 -0300

    [v0.2] simple signup implemented.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div id="footer-div">
+      <p>2021. Manoel e Raul</p>
+    </div>
+  );
+}
+
+export default Footer;
