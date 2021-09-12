b93d844dc28d8ab16621cad83de0744f605a775c
Raul Guimaraes <raul.metalurgica134@gmail.com>
2021-09-12 15:30:54 -0300

    random stocks add

--- src/components/InvestmentList/StockHomepage.js
+++ src/components/InvestmentList/StockHomepage.js
@@ -2,53 +2,54 @@ import React from "react";
 import axios from "axios";
 import Chart from "chart.js/auto";
 import "./StockHomepage.css";
-import InvestmentSearchList from "./InvestmentSearchList";
 
-const randomStockList = [
-  "AAPL",   "MSFT",   "AMZN",   "FB",   "GOOG",   "GOOGL",   "TSLA",   "NVIDIA",
-  "PYPL",   "ASML",   "INTC",   "NFLX",   "ADBE",   "CSCO",   "PEP",   "XOM",
-  "C",   "PFE",   "GE",   "AIG",   "WMT"];
+
+const randomStockList = ["AAPL", "MSFT", "AMZN", "FB", "GOOG", "GOOGL"]                  
 
 class StockHomepage extends React.Component {
-  state = {
-    chartValuesX: [],
-    chartValuesY: [],
-    companySymbol: "FB",
-    outputsize: "compact",
-    typeInformation: "TIME_SERIES_DAILY",
-    isLoaded: null,
-    searchEndPoint: "",
-    bestMatches: [],
-  };
+     
+    state = {
+
+      chartValuesX: [],
+      chartValuesY: [],
+      companySymbol: "",
+      outputsize: "compact",
+      typeInformation: "TIME_SERIES_DAILY",
+      isLoaded: null,
+      searchEndPoint:"",
+      bestMatches:[],
+        
+  }
 
   getRandomStocks = () => {
-    let randomStocks = [];
-    for (let i = 0; i < 5; i++) {
-      randomStocks.push(
-        randomStockList[Math.floor(Math.random() * randomStockList.length)]
-      );
-    }
-    console.log(randomStocks);
-  };
+    const m = Math.floor(Math.random()* randomStockList.length)
+    return  randomStockList[m]
+}
 
   getChartData = async () => {
     const apiKey = "R2P4F9RG0EKKWZEU";
+    const random = this.getRandomStocks()
+       
 
-    // let url = `https://www.alphavantage.co/query?function=${
-    //   this.state.typeInformation
-    // }&symbol=${this.state.companySymbol.toUpperCase()}&outputsize=${
-    //   this.state.outputsize
-    // }&apikey=${apiKey}`;
-    // console.log(url)
+    let url = `https://www.alphavantage.co/query?function=${
+      this.state.typeInformation
+    }&symbol=${random}&outputsize=${
+      this.state.outputsize
+    }&apikey=${apiKey}`;
+    console.log(url)
 
     //teste para não passar do limite de requisições
-    let url =
-      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo%22";
+    // let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo%22"
 
     const response = await axios.get(url);
 
+    this.setState({
+    companySymbol: random
+    });
+
     this.transformDataChart(response.data);
   };
+
 
   // getCompanyData = async () => {
   //   let url2 = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=demo%27"
@@ -68,6 +69,7 @@ class StockHomepage extends React.Component {
     chartYclone.reverse();
 
     this.setState({
+     
       chartValuesX: [...chartXclone],
       chartValuesY: [...chartYclone],
     });
@@ -77,23 +79,25 @@ class StockHomepage extends React.Component {
     try {
       await this.getChartData();
       this.renderChart();
-      this.getRandomStocks();
+      this.getRandomStocks()
+     
+
     } catch (err) {
       console.error(err);
     }
   };
 
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.chartValuesY !== this.state.chartValuesY) {
-      this.renderChart();
-    }
-  };
+//   componentDidUpdate = (prevProps, prevState) => {
+//     if (prevState.chartValuesY !== this.state.chartValuesY) {
+//       this.renderChart();
+//     }
+//  }
+
+  
 
   renderChart = () => {
     if (this.state.chartValuesX.length === 0) {
-      return alert(
-        "Couldn't find information about this stock at the moment. Please try again or serch for another"
-      );
+      return alert("Couldn't find information about this stock at the moment. Please try again or serch for another");
     }
 
     if (this.state.isLoaded) {
@@ -114,7 +118,7 @@ class StockHomepage extends React.Component {
             fill: true,
             tension: 0.2,
             borderWidth: 1,
-            pointRadius: 1,
+            pointRadius:1,
           },
         ],
       },
@@ -137,16 +141,35 @@ class StockHomepage extends React.Component {
     this.setState({ isLoaded: chart });
   };
 
+
   render() {
     return (
-       
-
-        
-          <div className="canvasGraphic">
-            <canvas id="myCanvas"> </canvas>
+       <div className = "container-infoHomepage">
+                        
+         <div className= "container-stockInfo">
+         <div><h1>Stock of the day: {this.state.companySymbol}</h1></div>  
+            <canvas className = "container-graphStockDay" id="myCanvas"> </canvas>
+            <div className  =" cointainer-listInformations">
+            <div className = "stripInformation"> <b>Name:</b> Facebook Inc</div>
+            <div className = "stripInformation"><b>Exchange: </b>NASDAQ</div>
+            <div className = "stripInformation"><b>AssetType:</b> Common Stock</div>
+            <div className = "stripInformation"> <b>Sector: </b>Technology</div>
+            <div className = "stripInformation"><b>MarketCapitalization:</b> 1067693769000</div>
+            <div className = "stripInformation"><b>DividendPerShare:</b> None </div>
+            </div>
           </div>
-        
-     
+
+
+
+
+
+
+
+
+          <div className ="container-SigmaInfo">
+            <h1>TESTE</h1>
+          </div>
+        </div>
     );
   }
 }
